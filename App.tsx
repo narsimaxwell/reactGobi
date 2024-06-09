5ad1625e7ddcb1b20e838f238d84ae81e8ea4832
narsimaxwell <149683470+narsimaxwell@users.noreply.github.com>
2024-06-09 22:52:57 +0530

    Update and rename App.js to App.tsx

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Calendar from './Calendar';
 
-function App() {
+const App: React.FC = () => {
     return (
         <div className="App">
             <Calendar />
         </div>
     );
-}
+};
 
 export default App;
